5c121f76fee6a0078c27e7d855d32d598f03e2d7
Jerry12345-ss <justway12345@gmail.com>
2025-04-24 16:46:36 +0800

    尚未修正訊息特殊CSS問題和研究滾輪程式碼 先上第一版

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,81 @@
 <template>
-  <div class="container mx-auto px-4 my-4">
-    <div class="flex flex-col justify-center items-center gap-2">
-      <h1 class="text-3xl !font-medium py-4 text-gray-700">簡易聊天室</h1>
-      
-      <!-- 會動的日期與時間 + 用戶名稱 -->
-      <div class="flex flex-col gap-2">
-        <div class="text-gray-600">用戶名稱 : {{ username }}</div>
-        <div>當前時間 : </div>
-      </div>
+  <div class="container mx-auto max-w-[950px] min-h-screen px-4 py-12">
+    <div class="containers w-full flex items-center justify-center rounded">
+      <div class="flex flex-col md:flex-row justify-around items-center gap-4 w-full p-4 py-6">
+        <!-- 資訊欄 -->
+        <div class="chatroom-info flex flex-col gap-2 items-center md:items-start md:w-[270px]">
+          <h1 class="text-3xl !font-medium py-4 text-gray-700 pt-8">簡易公開聊天室</h1>
 
-      <div class="chat-container h-[800px] w-full min-w-[330px] max-w-[500px] rounded border border-gray-300">
-        <div class="flex flex-col justify-between h-full relative">
-          <div class="messages-container flex-1 overflow-y-auto" ref="messageContainer" @scroll="handleScroll">
-            <div class="p-4">
-              <div class="flex flex-col gap-4">
-                <!-- 這裏針對 css 還要處理 -->
-                <div v-for="message in messages" :key="message.time" :id="message.time">
-                  <div class="flex flex-col" :class="{'max-w-[70%]' : message.type !== 'system', 'ml-auto items-end' : message.type === 'myself', 'mr-auto items-start' : message.type === 'other' }">
-                    <div class="p-4 rounded inline-block h-auto w-full message-content" 
-                      :class="[messageBgColor(message), { '!w-fit' : message.type !== 'system' }]"
-                      >
-                      <div class="flex flex-col">
-                        <div class="flex items-center gap-2 flex-wrap">
-                          <div class="text-gray-600">{{ message.sender }} : </div>
-                          <div class="overflow-hidden break-words text-gray-600">{{ message.content }}</div>
+          <div class="flex flex-col gap-2 flex-wrap items-center md:items-start">
+            <div class="flex gap-1">
+              <div>用戶名稱 :</div>
+              <div>{{ username }}</div>
+            </div>
+            <div class="flex flex-wrap gap-1">
+              <div>當前時間 :</div>
+              <DynamicDateTime/>
+            </div>
+  
+            <!-- 連線狀態 -->
+            <div class="status-container">
+              <div class="flex gap-1 flex-wrap" :class="['status', { connect : isConnected }]">
+                <div>連線狀態 :</div>
+                <div :style="{ color : connectStatus.color }" class="font-medium">{{ connectStatus.text }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- 聊天室 -->
+        <div class="chat-container h-[850px] w-full min-w-[280px] max-w-[500px] rounded border border-gray-300">
+          <div class="flex flex-col justify-between h-full relative">
+            <div class="messages-container flex-1 overflow-y-auto" ref="messageContainer" @scroll="handleScroll">
+              <div class="p-4">
+                <div class="flex flex-col gap-4">
+                  <!-- 這裏針對 css 還要處理 -->
+                  <div v-for="message in messages" :key="message.time" :id="message.time">
+                    <div class="flex flex-col" :class="{'max-w-[70%]' : message.type !== 'system', 'ml-auto items-end' : message.type === 'myself', 'mr-auto items-start' : message.type === 'other' }">
+                      <div class="p-4 rounded inline-block h-auto w-full message-content" 
+                        :class="[messageBgColor(message), { '!w-fit' : message.type !== 'system' }]"
+                        >
+                        <div class="flex flex-col">
+                          <div class="flex items-center gap-2 flex-wrap">
+                            <div class="text-gray-600">{{ message.sender }} : </div>
+                            <div class="overflow-hidden break-words text-gray-600">{{ message.content }}</div>
+                          </div>
+                          
+                          <div class="text-xs text-gray-400 mt-2">{{ message.time }}</div>
                         </div>
-                        
-                        <div class="text-xs text-gray-400 mt-2">{{ message.time }}</div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
+              <!-- 當有未讀訊息時, 向下箭頭會消失 -->
+              <!-- 未讀訊息 -->
+              <template v-if="!isAtBottom && !showUnRead">
+                <div class="absolute bottom-24 right-8 z-[100000]">
+                  <button @click="scrollToBottom" style="box-shadow: 0 0 15px 3px rgba(0, 0, 0, .2);" class="cursor-pointer w-[40px] h-[40px] bg-[#adb163] rounded flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: white;transform: ;msFilter:;"><path d="m18.707 12.707-1.414-1.414L13 15.586V6h-2v9.586l-4.293-4.293-1.414 1.414L12 19.414z"></path></svg>
+                  </button>
+                </div>
+              </template>
+
+              <template v-if="showUnRead && unReadData?.type !== 'system'">
+                <div class="h-14 p-4 unread-container absolute bottom-20 w-full z-[100000] cursor-pointer" @click="scrollToBottom">
+                  <div class="text-gray-600 truncate">{{ unReadData.sender }} : {{ unReadData.content }}</div>
+                </div>
+              </template>
             </div>
-            <!-- 當有未讀訊息時, 向下箭頭會消失 -->
-            <!-- 未讀訊息 -->
-            <template v-if="!isAtBottom && !showUnRead">
-              <div class="absolute bottom-24 right-8 z-[100000]">
-                <button @click="scrollToBottom" style="box-shadow: 0 0 15px 3px rgba(0, 0, 0, .2);" class="cursor-pointer w-[40px] h-[40px] bg-[#adb163] rounded flex items-center justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: white;transform: ;msFilter:;"><path d="m18.707 12.707-1.414-1.414L13 15.586V6h-2v9.586l-4.293-4.293-1.414 1.414L12 19.414z"></path></svg>
-                </button>
-              </div>
-            </template>
 
-            <template v-if="showUnRead && unReadData?.type !== 'system'">
-              <div class="h-14 p-4 unread-container absolute bottom-20 w-full z-[100000] cursor-pointer" @click="scrollToBottom">
-                <div class="text-gray-600 truncate">{{ unReadData.sender }} : {{ unReadData.content }}</div>
+            <div class="input-container h-20 p-4 border-[#adb163] bg-[#cbce99] ">
+              <div class="flex gap-4 h-full">
+                <!-- 這裡要用 keypress, 不能用 keydown(差異在於中文輸入有無問題) -->
+                <input v-model.trim="clientMessage" @keypress.enter="sendMessage" :disabled="!isConnected" :class="{ 'cursor-not-allowed' : !isConnected }" type="text" :placeholder="!isConnected ? '連線發生錯誤 ,暫時無法輸入' : '輸入訊息...'" class="h-full border border-[#adb163] rounded bg-[#eff1c9] flex-1 outline-none px-4">
+                <button @click="sendMessage" :disabled="!isConnected" :class="[!isConnected ? 'cursor-not-allowed' : 'cursor-pointer', { disabled : !isConnected }]" class="hidden sm:block message-btn px-4 py-2 text-white bg-[#949755] rounded transition-colors hover:bg-[#6a6c3d]">發送</button>
               </div>
-            </template>
-          </div>
-
-          <div class="input-container h-20 p-4 border-[#adb163] bg-[#cbce99] ">
-            <div class="flex gap-4 h-full">
-              <!-- 這裡要用 keypress, 不能用 keydown(差異在於中文輸入有無問題) -->
-              <input v-model.trim="clientMessage" @keypress.enter="sendMessage" type="text" :placeholder="!isConnected ? '連線發生錯誤 ,暫時無法輸入' : '輸入訊息...'" class="h-full border border-[#adb163] rounded bg-[#eff1c9] flex-1 outline-none px-4">
-              <button @click="sendMessage" :disabled="!isConnected" :class="[!isConnected ? 'cursor-not-allowed' : 'cursor-pointer', { disabled : !isConnected }]" class="message-btn px-4 py-2 text-white bg-[#949755] rounded transition-colors hover:bg-[#6a6c3d]">發送</button>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div class="status-container">
-        <div class="flex gap-2 items-center py-2" :class="['status', { connect : isConnected }]">
-          <div>連線狀態 :</div>
-          <div :style="{ color : connectStatus.color }">{{ connectStatus.text }}</div>
         </div>
       </div>
     </div>
@@ -73,6 +85,7 @@
 <script setup>
 import { onMounted, onUnmounted, ref, reactive, useTemplateRef, nextTick } from 'vue';
 import { messageBg } from './data/message';
+import DynamicDateTime from './component/DynamicDateTime.vue';
 
 const socket = ref(null);
 const clientMessage = ref('');
@@ -143,12 +156,16 @@ const connectWebSocket = () =>{
     // }else{
     //   messageContainer.value.scrollTop = messageContainer.value.scrollHeight;
     // }
-    if (isAtBottom.value) {
+    if(message.type === 'system'){
       showUnRead.value = false;
-      scrollToBottom(); // 如果滾動條在底部，自動滾動到底部
     }else{
-      showUnRead.value = true;
-      unReadData.value = message;
+      if (isAtBottom.value) {
+        showUnRead.value = false;
+        scrollToBottom(); // 如果滾動條在底部，自動滾動到底部
+      }else{
+        showUnRead.value = true;
+        unReadData.value = message;
+      } 
     }
   }
 
@@ -203,6 +220,10 @@ const handleScroll = (event) =>{
 </script>
 
 <style lang="scss" scoped>
+.containers{
+  background-color: rgba(234, 233, 233, 0.6);
+}
+
 .chat-container{
   background-image: url('../src/assets/img/chatroom-bg.jpg');
   background-position: center;
@@ -229,7 +250,6 @@ const handleScroll = (event) =>{
 <!-- 
   1. 針對於訊息的部分, CSS 要額外處理... (最麻煩)
   2. 針對於滾輪那裡的程式碼要稍微研究一下
-  3. 版面的部分要調整, 打算將最上面擺到左邊(資訊欄), 右邊擺設聊天視窗
 
   ** 如果還要新增其他功能, 比如說與 Firebase 串接, 製作自創建使用者、聊天訊息儲存(依照日期時間儲存)之類的, 更甚至傳送圖片之類的, 這些都可以再進行擴充 
 -->
---
+++ src/component/DynamicDateTime.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        {{ currentTime.toLocaleString() }}
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const currentTime = ref(new Date());
+let timer = null;
+
+onMounted(()=>{
+    timer = setInterval(updateTimee, 1000); 
+});
+
+onUnmounted(()=>{
+    if(timer){
+        clearInterval(timer);
+    } 
+});
+
+// 更新當前時間
+const updateTimee = () =>{
+    currentTime.value = new Date(); 
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
